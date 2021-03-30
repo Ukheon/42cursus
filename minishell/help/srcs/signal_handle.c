934dd148a98e2aa315acc792cebef7585365d080
Ukheon Kwon <dnrgjs33@naver.com>
2021-03-30 19:21:31 +0900

    2021-03-30

---
+++ minishell/help/srcs/signal_handle.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal_handle.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeonski <hyeonski@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 11:28:09 by hyeonski          #+#    #+#             */
+/*   Updated: 2021/02/11 16:45:11 by hyeonski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	handle_signal_main(int signo)
+{
+	if (signo == SIGINT)
+	{
+		errno = 1;
+		ft_putstr_fd("\b\b  \b\b", 1);
+		ft_putstr_fd("\n", STDERR_FILENO);
+		print_prompt();
+		signal(SIGINT, handle_signal_main);
+	}
+	else if (signo == SIGQUIT)
+	{
+		ft_putstr_fd("\b\b  \b\b", 1);
+	}
+}
+
+void	handle_signal_chlid(int signo)
+{
+	if (signo == SIGINT)
+	{
+		ft_putstr_fd("\n", STDERR_FILENO);
+		signal(SIGINT, handle_signal_main);
+	}
+	if (signo == SIGQUIT)
+	{
+		ft_putstr_fd("Quit: 3\n", STDERR_FILENO);
+		signal(SIGQUIT, handle_signal_main);
+	}
+}
--- minishell/main.c
+++ minishell/main.c
@@ -1,37 +1,114 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
+#include "get_next_line.h"
+#include "../libft/libft.h"
+#include <string.h>
+#include <sys/wait.h>
 
-int main(void)
+void	unset_list(t_list **head, char *str)
 {
-	pid_t pid; //pid_t 선언
+	t_list *temp;
+	t_list *temp2;
+	t_list *prev;
 
-	pid = fork(); //fork 발생
-	if(pid == -1) { //-1 이면 fork생성 에러
-		printf("can't fork, erro\n");
-		exit(0);
-	}
-    printf("start : ============================================\n");
-    printf("pid = %d\n",pid);
-	if(pid == 0) { //0이면 자식 프로세스
-		int j;
-		for(j = 0; j < 10; j++) {
-			printf("child: %d\n", j);
+	temp = *head;
+	while (temp->next)
+	{
+		if ((ft_strncmp(temp->key, str, ft_strlen(temp->key)) == 0))
+		{
+			temp2 = temp->next;
+			prev->next = temp2;
+			ft_lstdelone(temp, free);
 		}
-        // sleep(5);
-        printf("child pid = %d\n",pid);
-	} else { //부모프로세스
-		int i;
-		for(i = 0; i < 10; i++) {
-			printf("parent : %d\n", i);
-            // sleep(1);
-		}
-        printf("why?\n");
-        printf("parent pid = %d\n",pid);
+		prev = temp;
+		temp = temp->next;
 	}
-    printf("pid = %d\n",pid);
-    printf("end : ============================================\n");
-	return 0;
+}
+
+void	check_list(t_list *head)
+{
+	t_list *temp;
+
+	while (head->next)
+	{
+		printf("%s%c%s\n",head->key, '=', head->value);
+		head = (head)->next;
+	}
+}
+
+
+void	init_list(t_list **head, char *envp[])
+{
+	int			i;
+
+	i = 0;
+	while (envp[i])
+	{
+		ft_lstadd_back(head, ft_lstnew(envp[i]));
+		i++;
+	}
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	t_list *head;
+	int len;
+	
+	head = NULL;
+	init_list(&head, envp);
+	len = ft_lstsize(head);
+	printf("%d\n\n",len);
+	check_list(head);
+	unset_list(&head, "hi");
+	len = ft_lstsize(head);
+	printf("%d\n\n",len);
+	char *str;
+	while (head->next)
+	{
+		printf("%s=%s\n",head->key, head->value);
+		head = head->next;
+	}
+	int fd;
+	char *line;
+	int id;
+	int stay;
+	int i = 0;
+	fd = 0;
+	int ret;
+	(void)argv;
+	write(1, "$>", 2);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		if (strncmp(line, "ls", 2) == 0)
+		{
+			
+			id = fork();
+			if (id != 0)
+			{
+				printf("parent here ? \n");
+				wait(&id);
+				printf("parent id %d\n", id);
+			}
+			else if (id == 0)
+			{
+				execve("/bin/ls",argv, envp);
+				exit(0);
+			}
+		}
+
+		if (strncmp(line, "cat ", 4) == 0)
+		{
+			char *str;
+			int a;
+			char *l;
+			str = strchr(line, ' ');
+			a = open(str + 1, O_RDONLY);
+			while (get_next_line(a, &l) > 0)
+			{
+				printf("%s\n", l);
+			}
+			printf("%s\n", l);
+		}
+		write(1, "$>", 2);
+	}
+	return (0);
 }
