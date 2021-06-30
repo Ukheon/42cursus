b38f4e9a460d3145a57c64d8f44d935439b7223f
ukheon <dnrgjs33@naver.com>
2021-06-30 17:24:06 +0900

    gogo

--- CPP/C1/ex07/main.cpp
+++ CPP/C1/ex07/main.cpp
@@ -11,14 +11,21 @@ int				main(int argc, char **argv)
 		std::string		buf;
 		std::fstream	res;
 		std::string		re_file(argv[1]);
+		std::string::size_type pos = 0;
+		std::string s1(argv[2]);
+		std::string s2(argv[3]);
 
 		re_file.append(".replace");
 		res.open(re_file, std::ios::out);
 		file.open(argv[1], std::ios::in);
 		while (getline(file, buf))
 		{
-			while ((int)buf.find(argv[2]) >= 0)
-				buf = buf.replace(buf.find(argv[2]), strlen(argv[2]), argv[3]);
+			pos = 0;
+			while ((pos = buf.find(s1, pos)) != std::string::npos)
+			{
+				buf = buf.replace(pos, s1.size(), s2);
+				pos += s2.size();
+			}
 			res << buf << std::endl;
 		}
 		res.close();
