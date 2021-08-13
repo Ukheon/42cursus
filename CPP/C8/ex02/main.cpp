246a7162c433ec539aa3af121889f0a1cf28983e
Ukwon <dnrgjs33@naver.com>
2021-08-13 22:51:25 +0900

    finish C8

--- CPP/C8/ex02/main.cpp
+++ CPP/C8/ex02/main.cpp
@@ -1,7 +1,8 @@
 #include "mutantstack.hpp"
 #include <vector>
 
-int main()
+
+void subjectTest()
 {
 	MutantStack<int> mstack;
 	mstack.push(5);
@@ -12,7 +13,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -20,10 +20,42 @@ int main()
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
-	return 0;
+}
+
+void myTest()
+{
+	MutantStack<int> s;
+
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	std::cout << s.top() << std::endl;
+	s.pop();
+	std::cout << s.top() << std::endl;
+	s.pop();
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);
+	s.push(60);
+	std::cout << "size : " << s.size() << std::endl;
+	std::cout << "now top : " << s.top() << std::endl;
+	MutantStack<int>::reverse_iterator rp;
+	rp = s.rbegin();
+	for (; rp != s.rend(); ++rp)
+	{
+		std::cout << *rp << std::endl;
+	}
+}
+
+int main()
+{
+	subjectTest();
+	std::cout << " ========================== " << std::endl;
+	myTest();
 }
 
